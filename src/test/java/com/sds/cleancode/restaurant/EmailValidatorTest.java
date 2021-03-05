ede3db8e308a98977a7e62f028ad8063499ff126
su-ryeon.lee <su-ryeon.lee@samsung.com>
2021-03-05 17:53:44 +0900

    add email validation test

--- src/test/java/com/sds/cleancode/restaurant/EmailValidatorTest.java
+++ src/test/java/com/sds/cleancode/restaurant/EmailValidatorTest.java
@@ -10,23 +10,18 @@ public class EmailValidatorTest {
 
     @Test
     public void 이메일에_골뱅이가없으면_false를리턴한다(){
-        assertThat(EmailValidator.isValid("email-without-at"), is(false));
-        //then(EmailValidator.isValid("email-without-at")).isFalse();
     }
 
     @Test
     public void 이메일에_특수문자가있으면_false를리턴한다(){
-        assertThat(EmailValidator.isValid("email-with-#"), is(false));
     }
 
     @Test
     public void 이메일의_골뱅이뒤에_도메인주소가없으면_false를리턴한다(){
-        assertThat(EmailValidator.isValid("email-without-domain@"), is(false));
     }
 
     @Test
     public void 이메일이_유효하면_true를리턴한다(){
-        assertThat(EmailValidator.isValid("email@email.com"), is(true));
     }
 }
 
