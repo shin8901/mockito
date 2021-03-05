296aecf5f331e7485ef78139ee755bf18b7ad684
su-ryeon.lee <su-ryeon.lee@samsung.com>
2021-03-05 17:49:25 +0900

    add email validation test

--- src/test/java/com/sds/cleancode/restaurant/EmailValidatorTest.java
+++ src/test/java/com/sds/cleancode/restaurant/EmailValidatorTest.java
@@ -2,6 +2,7 @@ package com.sds.cleancode.restaurant;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.BDDAssertions.then;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -9,30 +10,23 @@ public class EmailValidatorTest {
 
     @Test
     public void 이메일에_골뱅이가없으면_false를리턴한다(){
-        boolean result = EmailValidator.isValid("email-without-at");
-
-        assertThat(result, is(false));
+        assertThat(EmailValidator.isValid("email-without-at"), is(false));
+        //then(EmailValidator.isValid("email-without-at")).isFalse();
     }
 
     @Test
     public void 이메일에_특수문자가있으면_false를리턴한다(){
-        boolean result = EmailValidator.isValid("email-with-#");
-
-        assertThat(result, is(false));
+        assertThat(EmailValidator.isValid("email-with-#"), is(false));
     }
 
     @Test
     public void 이메일의_골뱅이뒤에_도메인주소가없으면_false를리턴한다(){
-        boolean result = EmailValidator.isValid("email-without-domain@");
-
-        assertThat(result, is(false));
+        assertThat(EmailValidator.isValid("email-without-domain@"), is(false));
     }
 
     @Test
     public void 이메일이_유효하면_true를리턴한다(){
-        boolean result = EmailValidator.isValid("email@email.com");
-
-        assertThat(result, is(true));
+        assertThat(EmailValidator.isValid("email@email.com"), is(true));
     }
 }
 
