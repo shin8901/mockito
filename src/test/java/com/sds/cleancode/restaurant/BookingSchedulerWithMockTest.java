0a90244c0c8cb31028ec9f7286e1c345fcf6f215
su-ryeon.lee <su-ryeon.lee@samsung.com>
2021-03-06 23:52:15 +0900

    add test scenario with mock

---
+++ src/test/java/com/sds/cleancode/restaurant/BookingSchedulerWithMockTest.java
@@ -0,0 +1,27 @@
+package com.sds.cleancode.restaurant;
+
+import org.junit.Test;
+
+public class BookingSchedulerWithMockTest {
+
+    @Test
+    public void 예약완료시_SMS는_무조건_발송() {
+    }
+
+    @Test
+    public void 이메일이_없는_경우에는_이메일_미발송() {
+    }
+
+    @Test
+    public void 이메일이_있는_경우에는_이메일_발송() {
+    }
+
+    @Test
+    public void 현재날짜가_일요일인_경우_예약불가_예외처리() {
+    }
+
+    @Test
+    public void 현재날짜가_일요일이_아닌경우_예약가능() {
+    }
+
+}
